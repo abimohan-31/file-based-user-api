61789b44e6209840879d8ff47a19733b64a1eff2
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-09-20 21:59:19 +0530

    setup express app

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoutes);
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

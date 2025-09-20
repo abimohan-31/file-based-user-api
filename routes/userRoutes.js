535f7f13abfadf2e0c206052781a0a0af289ac4c
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-09-20 22:02:56 +0530

    route created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,4 +7,5 @@ router.get("/:id", userController.getUserById);
 router.post("/", userController.createUser);
 router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
+
 module.exports = router;

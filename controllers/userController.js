2936836b775ad849f1c77be44369e6f09963e09f
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-02 23:53:14 +0530

    userController added but still not workiing

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+import { readFileSync, writeFileSync } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePath = path.join(__dirname, "../data/users.json");
+
+function readUsers() {
+  const data = readFileSync(filePath, "utf8");
+  return JSON.parse(data);
+}
+
+function writeUsers(users) {
+  writeFileSync(filePath, JSON.stringify(users, null, 2));
+}
+
+export const getAllUsers = (req, res) => {
+  const users = readUsers();
+  res.json(users);
+};
+
+export const getUserById = (req, res) => {
+  const users = readUsers();
+  const user = users.find((p) => p.id === parseInt(req.params.id));
+  if (!user) return res.status(404).json({ error: "user not found" });
+  res.send(user);
+};
+
+export const createUser = (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ error: "Name and email are required" });
+  }
+
+  const users = readUsers();
+
+  // Create new user
+  const newUser = {
+    id:
+      users.length > 0 ? Math.max(...users.map((u) => parseInt(u.id))) + 1 : 1,
+    name,
+    email,
+  };
+
+  users.push(newUser);
+
+  writeUsers(users);
+
+  res.status(201).json({ message: "User added Successfully" });
+};

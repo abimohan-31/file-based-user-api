fa2a32783538664b9f03af4748fe67991b6666ae
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-02 23:51:10 +0530

    format changed

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from "express";
+// import bodyParser from "body-parser";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Express test");
+// });
 
 const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(`Server is running in http://localhost:${PORT}`)
+);

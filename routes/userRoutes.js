5f2494f3d6739c1e15f1a396ac1906cfc918c23b
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-04 19:43:15 +0530

    checked

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,15 @@ import {
   getAllUsers,
   getUserById,
   createUser,
-  updateUser,
-  deleteUser,
+  // updateUser,
+  // deleteUser,
 } from "../controllers/userController.js";
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
 router.post("/", createUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+// router.put("/:id", updateUser);
+// router.delete("/:id", deleteUser);
 
 export default router;

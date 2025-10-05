e64dfb446225fffffe369cd4d4d46de21781a84a
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-10-05 10:22:57 +0530

    uncomment the updatedUser function

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ import {
   getAllUsers,
   getUserById,
   createUser,
-  // updateUser,
+  updateUser,
   // deleteUser,
 } from "../controllers/userController.js";
 const router = express.Router();
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get("/", getAllUsers);
 router.get("/:id", getUserById);
 router.post("/", createUser);
-// router.put("/:id", updateUser);
+router.put("/:id", updateUser);
 // router.delete("/:id", deleteUser);
 
 export default router;
